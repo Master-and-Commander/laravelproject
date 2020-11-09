426941e4ecd4baf70213b947c010fe3089ff17d8
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-11-09 05:37:19 -0500

    improve fish general looks

--- resources/views/fishGeneral.blade.php
+++ resources/views/fishGeneral.blade.php
@@ -4,9 +4,22 @@
 <!-- perhaps best to use a database again -->
 <div class="container"> 
     
-    <?php foreach ($fishes as $fish) : ?>
-      <p> hello {{$fish[1]}} </p>
-    <?php endforeach ?>
+<h2> Our Fish </h2>
+	<hr />
+	<div class= "row col-md-12">
+	<?php foreach ($fishes as $fish) : ?>
+		<div class = "card-row">
+		  <div class="card">
+				<div class="card-body">
+					<h5 class="card-title">{{$fish[1]}}</h5>
+					<hr class ="hr-card"/>
+					<p class="card-text">{{$fish[2]}}</p>
+					<a href="/fish/{{$fish[1]}}" class="btn btn-primary">Keep Reading</a>
+				</div>
+			</div>
+		</div>
+	<?php endforeach ?>
+	</div>
 
 </div>
 @endsection
